bbc32fd2dec895444764d3fe8f5eadfc87de9003
Sztivi-DK <szalontai.istvan@gmail.com>
2026-05-30 19:07:19 +0200

    Express installed and prepare

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Ready');
+const express = require('express');
+const app = express();
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>');
+});
+
+app.listen(8887);
