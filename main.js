565dcfb550efcd66b50556e33ac335f5168200a9
Sztivi-DK <szalontai.istvan@gmail.com>
2026-05-30 20:20:24 +0200

    Links and Routes - Example

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+
+app.use( express.static('public') );
+
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>');
+    response.send('<link rel="stylesheet" href="style.css"> <h1>Hello World!</h1> <a href="/valami">Add előre!</a>');
+});
+
+app.get('/valami', (request, response) => {
+    response.send('<link rel="stylesheet" href="style.css"> <h1>Valami!</h1> <a href="/">Vissza a jövőbe!</a>');
 });
 
 app.listen(8887);
