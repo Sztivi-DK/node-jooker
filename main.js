ff7648f1049473b806b9c3de0a2458bd8cbe75f0
Sztivi-DK <szalontai.istvan@gmail.com>
2026-06-01 15:52:14 +0200

    Express + EJS Rendering with Views

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use( express.static('public') );
 
 app.get('/', (request, response) => {
-    const ejs = require('ejs');
-    const template = '<link rel="stylesheet" href="style.css"> <h1><%= title %></h1> <p><%= text %></p> <a href="/valami">Add előre!</a>';
-    const model = {title: 'Kezdőlap', text: 'Ez pedig a főoldal tartalmi része'};
-    const html = ejs.render(template, model);
-    response.send(html);
+    const model = {title: 'Üdvözlünk'};
+    response.render('home', model);
 });
 
 app.get('/valami', (request, response) => {
-    response.send('<link rel="stylesheet" href="style.css"> <h1>Valami!</h1> <a href="/">Vissza a jövőbe!</a>');
+    const model = {title: 'Valami'};
+    response.render('home', model);
 });
 
 app.listen(8887);
