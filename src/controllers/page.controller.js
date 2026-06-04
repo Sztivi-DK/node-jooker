f9522f876eebbb394ef3f70095eb96abe392441b
Sztivi-DK <szalontai.istvan@gmail.com>
2026-06-04 19:17:59 +0200

    Home and Contacts Pages

--- src/controllers/page.controller.js
+++ src/controllers/page.controller.js
@@ -1,10 +1,12 @@
+const PageModel = require('../models/page.model');
+
 class PageController {
     static homePage(request, response) {
-        const model = {title: 'Üdvözlünk'};
-        response.render('pages/home', model);
+        const model = {title: 'Üdvözlünk', content: PageModel.getHomeContent()};
+        response.render('pages/page', model);
     }
     static contactsPage(request, response) {
-        const model = {title: 'Elérhetőségeink'};
+        const model = {title: 'Elérhetőségeink', content: PageModel.getContactContent()};
         response.render('pages/contacts', model);
     }
 }
